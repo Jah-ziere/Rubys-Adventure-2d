2b1c7c75028b8f5c6c4cdce89672e8cedf7eb531
Jah-ziere <146846048+Jah-ziere@users.noreply.github.com>
2024-02-08 08:44:27 -0500

    Dialog Raycast

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -49,13 +49,13 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = position.x + Time.deltaTime * speed;
+            position.y = position.x + Time.deltaTime * speed  * direction;
             animator.SetFloat("Move X", 0);
             animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed;
+            position.x = position.x + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
         }
